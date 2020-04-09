abab98a49ae7c251d8ab657d91476d62b05045ec
Emil Valentin <atomicpotato8@gmail.com>
2020-04-09 09:33:38 +0200

    Setup sensor
    The linear-acceleration sensor has been setup

--- ballthrowingapp/app/src/main/java/dk/aau/student/meda2a220astudent/ballthrowingapp/MainActivity.java
+++ ballthrowingapp/app/src/main/java/dk/aau/student/meda2a220astudent/ballthrowingapp/MainActivity.java
@@ -1,13 +1,38 @@
 package dk.aau.student.meda2a220astudent.ballthrowingapp;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private SensorManager linearAccelerationManager;
+    private Sensor linearAccelerationSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        linearAccelerationManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        linearAccelerationSensor = linearAccelerationManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+        linearAccelerationManager.registerListener(this, linearAccelerationSensor, SensorManager.SENSOR_DELAY_UI);
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
